0a5728756040a6e6a87ad15084961919f3671427
Besart Ramadani <besartr1995@gmail.com>
2026-03-25 18:42:39 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <GoogleAnalytics gaId="G-N04R25FKDK" />
     </>
   );
 }
